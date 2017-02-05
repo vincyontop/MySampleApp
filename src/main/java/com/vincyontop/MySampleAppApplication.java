86dd61414015db8b858595acd3af7364cc8aa892
Arora <varor14@LAMU02R2BGYG8WP.uhc.com>
2017-02-05 13:26:03 -0600

    Initial commit

---
+++ src/main/java/com/vincyontop/MySampleAppApplication.java
@@ -0,0 +1,12 @@
+package com.vincyontop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySampleAppApplication.class, args);
+	}
+}
