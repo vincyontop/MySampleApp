16768be5f1d7f5a40c563449a365bbbaac944865
Arora <varor14@LAMU02R2BGYG8WP.uhc.com>
2017-02-21 11:26:08 -0600

    Made changes for Config server

--- src/main/java/com/vincyontop/controller/GreetingController.java
+++ src/main/java/com/vincyontop/controller/GreetingController.java
@@ -2,22 +2,34 @@ package com.vincyontop.controller;
 
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vincyontop.model.Greeting;
 
+@RefreshScope
 @RestController
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    
+    @Value("${message:Test default}")
+    private String message;
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+    
+    @RequestMapping("/message")
+    public String showMessage() {
+        return this.message;
     }
 	
 	
